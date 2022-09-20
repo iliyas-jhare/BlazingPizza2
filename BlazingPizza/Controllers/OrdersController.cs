009617659655c811b69f0c2e2719e1edb3dcbf92
Jhare Mohammad (ETAS-DAP/XPC-UK) <jhm1yok@bosch.com>
2022-09-20 15:00:11 +0100

    Checkout page. OrderDetail page.  MainLayout.

--- BlazingPizza/Controllers/OrdersController.cs
+++ BlazingPizza/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using BlazingPizza.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace BlazingPizza.Controllers;
 
@@ -18,15 +19,11 @@ public class OrdersController : Controller
 
 	[HttpGet]
 	public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
-	{
-		var orders = await db.Orders
-			.Include(o => o.Pizzas).ThenInclude(p => p.Special)
-			.Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
-			.OrderByDescending(o => o.CreatedTime)
-			.ToListAsync();
-
-		return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
-	}
+		=> (await GetOrdersQuery(db.Orders)
+		.OrderByDescending(o => o.CreatedTime)
+		.ToListAsync())
+		.Select(o => OrderWithStatus.FromOrder(o))
+		.ToList();
 
 	[HttpPost]
 	public async Task<ActionResult<int>> PlaceOrder(Order order)
@@ -37,6 +34,17 @@ public class OrdersController : Controller
 		await db.SaveChangesAsync();
 		return order.OrderId;
 	}
+
+	[HttpGet("{orderId}")]
+	public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+		=> (await GetOrdersQuery(db.Orders.Where(o => o.OrderId == orderId)).SingleOrDefaultAsync()) is Order order
+		? OrderWithStatus.FromOrder(order)
+		: NotFound();
+
+	private IIncludableQueryable<Order, Topping> GetOrdersQuery(IQueryable<Order> orders)
+		=> orders
+		.Include(o => o.Pizzas).ThenInclude(p => p.Special)
+		.Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping);
 
 	private void UpdateSpecialAndSpecialId(Pizza pizza)
 	{
